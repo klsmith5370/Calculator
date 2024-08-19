dd489a84d67530bc715abd5760b0de6480f86892
Kourtney Smith <klsmith5370@gmail.com>
2024-08-19 17:22:53 -0400

    Created functionality for math functions

--- src/App.js
+++ src/App.js
@@ -15,38 +15,38 @@ function App() {
   }; 
  
   function minus(e) { 
-  	e.preventDefault();
-    setResult((result) => result + Number())
+    e.preventDefault(); 
+    setResult((result) => result - Number(inputRef.current.value));
   };
  
   function times(e) {
     e.preventDefault(); 
-    // Add the code for the plus function 
+    setResult((result) => result * Number(inputRef.current.value));
   }; 
  
   function divide(e) {
-    e.preventDefault();  
-    // Add the code for the divide function 
+    e.preventDefault(); 
+    setResult((result) => result / Number(inputRef.current.value));
   };
  
   function resetInput(e) {
-    e.preventDefault();  
-    // Add the code for the resetInput function 
+    e.preventDefault(); 
+    inputRef.current.value = 0; 
   }; 
  
   function resetResult(e) { 
     e.preventDefault(); 
-  	// Add the code for the resetResult function 
+    setResult((prevVal) => prevVal * 0);
   }; 
  
   return ( 
     <div className="App"> 
       <div> 
-        <h1>Simplest Working Calculator</h1> 
+        <h1>Calculator</h1> 
       </div> 
       <form> 
         <p ref={resultRef}> 
-          {/* add the value of the current total */} 
+          {result} 
         </p> 
         <input
           pattern="[0-9]" 
@@ -55,8 +55,8 @@ function App() {
           placeholder="Type a number" 
         /> 
         <button onClick={plus}>add</button>
-        <button onClick={subtract}>subtract</button>
-        <button onClick={multiply}>multiply</button>
+        <button onClick={minus}>subtract</button>
+        <button onClick={times}>multiply</button>
         <button onClick={divide}>divide</button>
         <button onClick={resetInput}>reset Input</button>
         <button onClick={resetResult}>reset Result</button> 
